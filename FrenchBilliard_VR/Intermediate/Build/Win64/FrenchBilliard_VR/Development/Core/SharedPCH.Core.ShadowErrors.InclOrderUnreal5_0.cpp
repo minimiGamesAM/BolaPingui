ab5ea15026ebfd57dce0abfc043a3c2e816725a2
Mauricio Medina <minimiGamesAM@gmail.com>
2023-09-04 18:25:16 -0400

    maybe we cannot need this.

--- FrenchBilliard_VR/Intermediate/Build/Win64/FrenchBilliard_VR/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30151
-#include "E:/Unreal/Projects_2023/French_BillarVR/Unreal/BolaPingui/FrenchBilliard_VR/Intermediate/Build/Win64/FrenchBilliard_VR/Development/Core/SharedPCH.Core.ShadowErrors.InclOrderUnreal5_0.h"
--- FrenchBilliard_VR/Intermediate/Source/FrenchBilliard_VR.cpp
+++
@@ -1,4 +0,0 @@
-#include "CoreTypes.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, FrenchBilliard_VR, "FrenchBilliard_VR");
